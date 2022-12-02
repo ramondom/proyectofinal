fe3be8ce73882c47819d7da77623c599334d5a74
ramondom <dominguezramon577@gmail.com>
2022-12-02 18:46:08 -0300

    act_tablas

--- srcipt_db.sql
+++ srcipt_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE "Peliculas" (
 	"actores"	TEXT,
 	"duracion"	TEXT,
 	PRIMARY KEY("id" AUTOINCREMENT)
-);
+)
 
 
 CREATE TABLE "Usuarios" (
@@ -15,18 +15,23 @@ CREATE TABLE "Usuarios" (
 	"contraseña"	TEXT,
 	"nombre"	TEXT,
 	"apellido"	TEXT,
-	"esActivo"	INTEGER,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
+	"esActivo"	INTEGER NOT NULL,
+	"mail"	REAL,
+	"dni"	TEXT,
+	"fechaNacimiento"	TEXT,
+	"idRol"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("idRol") REFERENCES "Roles"("idRol")
+)
 
 CREATE TABLE "Salas" (
 	"id"	INTEGER NOT NULL,
 	"nombre"	TEXT,
 	"filas"	INTEGER,
-	"butacas/fila"	INTEGER,
-    "estado"	INTEGER DEFAULT 1,
+	"butaca_fila"	INTEGER,
+	"tipo"	TEXT,
 	PRIMARY KEY("id" AUTOINCREMENT)
-);
+)
 
 CREATE TABLE "Reservas" (
 	"id"	INTEGER NOT NULL,
@@ -34,16 +39,50 @@ CREATE TABLE "Reservas" (
 	"sala"	INTEGER,
 	"pelicula"	INTEGER,
 	"usuario"	INTEGER,
-	"fechaReserva"	TEXT,
+	"fecha"	TEXT,
 	"precio"	INTEGER,
-    "estado"	INTEGER DEFAULT 0,
+	"estado"	INTEGER DEFAULT 0,
+	"butaca"	INTEGER,
+	FOREIGN KEY("butaca") REFERENCES "Butacas"("id"),
+	FOREIGN KEY("sala") REFERENCES "Salas"("id"),
+	FOREIGN KEY("pelicula") REFERENCES "Peliculas"("id"),
+	FOREIGN KEY("usuario") REFERENCES "Usuarios"("id"),
+	FOREIGN KEY("precio") REFERENCES "Descuentos"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
-);
+)
+
 
 CREATE TABLE "Descuentos" (
 	"id"	INTEGER NOT NULL,
 	"dia"	TEXT,
 	"tasa"	REAL,
-    "estado"	INTEGER DEFAULT 0,
+	"estado"	INTEGER DEFAULT 0,
 	PRIMARY KEY("id" AUTOINCREMENT)
-);
+)
+
+
+CREATE TABLE "Roles" (
+	"idRol"	INTEGER NOT NULL,
+	"nombre"	TEXT NOT NULL UNIQUE,
+	"activo"	INTEGER NOT NULL DEFAULT 1,
+	PRIMARY KEY("idRol")
+)
+
+
+CREATE TABLE "HistorialEntradas" (
+	"id"	INTEGER NOT NULL,
+	"usuario"	INTEGER,
+	"fecha"	TEXT,
+	"reserva"	INTEGER,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("usuario") REFERENCES "Usuarios"("id"),
+	FOREIGN KEY("reserva") REFERENCES "Reservas"("id")
+)
+
+CREATE TABLE "Butacas" (
+	"id"	INTEGER NOT NULL,
+	"fila"	INTEGER,
+	"columna"	INTEGER,
+	"estado"	INTEGER NOT NULL DEFAULT 1,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)
